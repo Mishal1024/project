311c87e35e97462f7cff3470aca3860007631d1e
Mishal S <mishals1810@gmail.com>
2026-06-25 12:43:55 +0530

    Updated project.py and test_project.py with structure

---
+++ project.py
@@ -0,0 +1,57 @@
+def main():
+    while True:
+        print("Menu\n1. Tasks\n2. Habits\n3. Notes\n4. Focus Sessions\n5. Stats\n6. Exit")
+        c = input("Choice: ")
+        match c:
+            case "1":
+                
+            case "2":
+
+            case "3":
+
+            case "4":
+
+            case "5":
+
+            case "6":
+                break
+            case _:
+                print("Invalid Input")
+                pass
+            
+
+def add_task():
+    ...
+
+
+def complete_task():
+    ...
+
+
+def add_habit():
+    ...
+
+
+def check_habit():
+    ...
+
+
+def add_note():
+    ...
+
+
+def search_notes():
+    ...
+
+
+def log_focus():
+    ...
+
+
+def calculate_stats():
+    ...
+
+
+
+if __name__ == "__main__":
+    main()
