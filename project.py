ba9d12d18bda2e70799257834c19b3b334bae7da
Mishal S <mishals1810@gmail.com>
2026-06-27 12:53:31 +0530

    Better use of OOP, changed loading and saving with to_list() and to_dict()

--- project.py
+++ project.py
@@ -11,10 +11,53 @@ class Task():
     def to_list(self):
         return [self.task,self.priority,self.done]
     
+    def to_dict(self):
+        return {
+            "task" : self.task,
+            "priority" : self.priority,
+            "done" : self.done
+        }
+    
+
+class Habit():
+    def __init__(self,habit,frequency,target):
+        self.habit = habit
+        self.frequency = frequency
+        self.target = target
+    
+    def to_list(self):
+        return [self.habit,self.frequency,self.target]
+    
+    def to_dict(self):
+        return {
+            "habit" : self.habit,
+            "frequency" : self.frequency,
+            "target" : self.target
+        }
+
 
 try:
     with open("data.json", "r") as file:
         data = json.load(file)
+        temp = []
+        for t in data["task"]:
+            task = Task(t["task"],t["priority"])
+            task.done = t["done"]
+            temp.append(task)
+        data["task"] = temp
+        temp = []
+        for h in data["habit"]:
+            habit = Habit(h["habit"],h["frequency"],h["target"])
+            temp.append(habit)
+        data["habit"] = temp
+        '''
+        temp = []
+        for n in data["note"]:
+            note = Task(n["title"],n["note"])
+            temp.append(note)
+        data["note"] = temp
+        '''
+
 except FileNotFoundError:
     data = {
         "task":[],
@@ -33,13 +76,13 @@ def main():
             case "1":
                 tasks = data["task"]
                 while True:
-                    print(tabulate(tasks,headers =["Task","Priority","Completion"],tablefmt="grid"))
+                    print(tabulate([task.to_list() for task in tasks],headers =["Task","Priority","Completion"],tablefmt="fancy_grid"))
                     print()
                     print("Tasks Menu\n1. Add Task\n2. Mark Complete\n3. Mark Incomplete\n4. Remove Completed\n5. Exit")
                     task_menu_choice = input("Choice: ")
                     match task_menu_choice:
                         case "1":
-                            add_task(tasks)
+                            add_task(input("Task: "),input("Priority: "),tasks)
                         case "2":
                             mark_task(int(input("Task Number: ")),tasks)
                         case "3":
@@ -53,8 +96,27 @@ def main():
                             print("Invalid Input")
                     print()
                     data["task"] = tasks
+
             case "2":
-                print("Habits")
+                habits = data["habit"]
+                while True:
+                    print(tabulate([habit.to_list() for habit in habits],headers =["Habit","Frequency","Target"],tablefmt="fancy_grid"))
+                    print()
+                    print("Habits Menu\n1. Add Habit\n2. Remove Habit\n3. Exit")
+                    habit_menu_choice = input("Choice: ")
+                    match habit_menu_choice:
+                        case "1":
+                            add_habit(input("Habit: "),input("Frequency: "),input("Target: "),habits)
+                        case "2":
+                            remove_habit(int(input("Task Number: ")),habits)
+                        case "3":
+                            print()
+                            break
+                        case _:
+                            print("Invalid Input")
+                    print()
+                    data["habit"] = habits
+
             case "3":
                 print("Notes")
             case "4":
@@ -66,23 +128,33 @@ def main():
             case _:
                 print("Invalid Input")
                 pass
+
+    data["task"] = [task.to_dict() for task in tasks]
+    data["habit"] = [habit.to_dict() for habit in habits]
     with open("data.json", "w") as file:
         json.dump(data,file,indent=4)
         file.close()
 
 
-def add_task(tasks):
-    tasks.append(Task(input("Task: "),input("Priority: ")).to_list())
+#Task functions
+def add_task(task,priority,tasks):
+    tasks.append(Task(task,priority))
 
 def mark_task(i,tasks):
-    tasks[i-1][2] = "Complete"
+    tasks[i-1].done = "Complete"
 
 def unmark_task(i,tasks):
-    tasks[i-1][2] = "Incomplete"
+    tasks[i-1].done = "Incomplete"
 
 def remove_marked(tasks):
-    return [task for task in tasks if task[2] != "Complete"]
+    return [task for task in tasks if task.done != "Complete"]
 
+#Habit functions
+def add_habit(habit,frequency,target,habits):
+    habits.append(Habit(habit,frequency,target))
+
+def remove_habit(i,habits):
+    del habits[i-1]
 
 
 if __name__ == "__main__":
