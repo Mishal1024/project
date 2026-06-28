41fcad5152314dfc961d03f259da7061ec018dab
Mishal S <mishals1810@gmail.com>
2026-06-28 16:52:39 +0530

    Added tests for add_task and mark_task

--- test_project.py
+++ test_project.py
@@ -1,10 +1,13 @@
-def test_function_1():
-    ...
+import project
 
 
-def test_function_2():
-    ...
+def test_add_task():
+    tasks = []
+    project.add_task("task","medium",tasks)
+    assert tasks[-1].task == "task" and tasks[-1].priority == "medium" and tasks[-1].done == "Incomplete"
 
-
-def test_function_n():
-    ...
+def test_mark_task():
+    tasks = []
+    project.add_task("task","medium",tasks)
+    project.mark_task(0,tasks)
+    assert tasks[-1].done == "Complete"
